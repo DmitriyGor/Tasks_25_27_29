4e7422f7226504f37159c6604ab35580757dbb65
Dmitriy <79133850099@yandex.ru>
2023-12-16 05:11:44 +0700

    Task 25

---
+++ Task_25/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 25: 
+Напишите цикл, который принимает на вход два числа (А и В) 
+и возводит число А в натуральную степень В.
+3, 5  -> 243 // (3 ^ 5)
+2, 4 -> 16   // (2 ^ 4)
+*/
+Console.Write("Enter a natural number A:  "); // Введите натуральное число А
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter a natural number B:  "); // Введите натуральное число B
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+for (int i = 1; i <= numberB; i++)
+{
+    result *= numberA;
+}
+Console.WriteLine ($"number {numberA} to natural power {numberB} is equal to: {result}"); 
+// натуральное число А в натуральной степени В равно:
