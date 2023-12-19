d4249a38b9621d84d826e00ad893a02aed8fdeef
Dmitriy <79133850099@yandex.ru>
2023-12-20 01:42:08 +0700

    Task 29

---
+++ Task_29/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+Console.WriteLine("Введите размер массива: ");
+int massive = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число N ");
+Console.WriteLine("будет задан массив из случайных чисел от 0 до N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int[]array = new int[massive];
+for (int i = 0; i < massive; i++)
+{
+    array[i] = new Random().Next(numberN+1);
+    
+}
+Console.WriteLine($"Массив: [ {String.Join("; ",array)} ]");
